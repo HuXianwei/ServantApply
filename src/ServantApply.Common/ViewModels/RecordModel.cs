2bfa2be12f012ed563aa373c531135896fcc3f36
Hxw <Administrator@AFOHC-608241908>
2017-03-28 00:22:53 +0800

    增加管理员审核报考记录功能

---
+++ src/ServantApply.Common/ViewModels/RecordModel.cs
@@ -0,0 +1,80 @@
+﻿using ServantApply.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServantApply.Common.ViewModels
+{
+    /// <summary>
+    /// 报名记录
+    /// </summary>
+    public class RecordModel
+    {
+        /// <summary>
+        /// 主键ID
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public long UserId { get; set; }
+
+        /// <summary>
+        /// 岗位ID
+        /// </summary>
+        public long JobId { get; set; }
+
+        /// <summary>
+        /// 审核人ID
+        /// </summary>
+        public long CheckId { get; set; }
+
+        /// <summary>
+        /// 报名时间
+        /// </summary>
+        public DateTime Time { get; set; }
+
+        /// <summary>
+        /// 报名状态
+        /// </summary>
+        public int Status { get; set; }
+
+        /// <summary>
+        /// 审核时间
+        /// </summary>
+        public DateTime CheckTime { get; set; }
+
+        /// <summary>
+        /// 审核备注
+        /// </summary>
+        public string Memo { get; set; }
+
+        /****************表外字段*****************/
+        /// <summary>
+        /// 报考用户名
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 岗位名
+        /// </summary>
+        public string JobName { get; set; }
+
+        public RecordModel() { }
+
+        public RecordModel(Record record)
+        {
+            Id = record.Id;
+            UserId = record.UserId;
+            JobId = record.JobId;
+
+            CheckId = record.CheckId;
+            Time = record.Time;
+            Status = record.Status;
+            CheckTime = record.CheckTime;
+            Memo = record.Memo;
+        }
+    }
+}
---
+++ src/ServantApply.WebApp/Controllers/RecordController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServantApply.Common;
+using ServantApply.Common.Enums;
+using ServantApply.Common.IManagers;
+using ServantApply.WebApp.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServantApply.WebApp.Controllers
+{
+    public class RecordController : Controller
+    {
+        private readonly IRecordManager recordManager;
+        public RecordController(IRecordManager recordManager)
+        {
+            this.recordManager = recordManager;
+        }
+
+        /// <summary>
+        /// 获取待审核的岗位列表
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> CheckJobList(int status)
+        {
+            var list = await recordManager.GetRecordsAsync(status);
+            ViewData["WaitCount"] = await recordManager.GetRecordCountAsync((int)JobCheckStatus.WaitCheck);
+            ViewData["SuccessCount"] = await recordManager.GetRecordCountAsync((int)JobCheckStatus.CheckSuccess);
+            ViewData["FailCount"] = await recordManager.GetRecordCountAsync((int)JobCheckStatus.CheckFail);
+            ViewData["Status"] = status;
+            return View(list);
+        }
+
+        /// <summary>
+        /// 审核通过
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="memo"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> CheckJobSuccess(long id, string memo)
+        {
+            ReturnResult result = await recordManager.CheckRecordAsync(id, memo, HttpContext.User.Identity.Uid(), (int)JobCheckStatus.CheckSuccess);
+            return Json(result);
+        }
+
+        /// <summary>
+        /// 审核不通过
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="memo"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> CheckJobFail(long id, string memo)
+        {
+            ReturnResult result = await recordManager.CheckRecordAsync(id, memo, HttpContext.User.Identity.Uid(), (int)JobCheckStatus.CheckFail);
+            return Json(result);
+        }
+    }
+}
